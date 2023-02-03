67cc03eaf1ccf1d5a89b98b7f63296b7356e4e5f
bwilliamson55 <benjaminawilliamson@gmail.com>
2023-02-02 20:06:38 -0500

    First pass at settings form with dynamic inputs

--- src/components/SettingsForm.vue
+++ src/components/SettingsForm.vue
@@ -1,30 +1,126 @@
 <template>
   <p>Value is {{ iSettingsString }}</p>
-
-  <div class="q-pa-md" style="max-width: 400px">
-    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-      <q-input
+  <p class="text-center">
+    Please see
+    <a
+      href="https://docs.meilisearch.com/reference/api/settings.html#settings-object"
+      >the meilisearch options reference</a
+    >
+    to understand available options.
+  </p>
+  <div class="q-pa-md">
+    <q-form @submit="onSubmit" class="q-gutter-md">
+      <q-select
         filled
-        v-model="name"
-        label="Some field label prop"
-        hint="Field hint prop"
-        lazy-rules
-        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+        v-model="iSettings.displayedAttributes"
+        label="Displayed Attributes"
+        hint="A list of strings, or *. Press enter to add values you type."
+        use-input
+        use-chips
+        multiple
+        stack-label
+        hide-dropdown-icon
+        input-debounce="0"
+        new-value-mode="add"
       />
-
-      <q-input
+      <q-select
         filled
-        type="number"
-        v-model="age"
-        label="Another field label prop"
-        lazy-rules
-        :rules="[
-          (val) => (val !== null && val !== '') || 'Please type things',
-          (val) =>
-            (val > 0 && val < 100) || 'Please type a number between 0 and 100',
-        ]"
+        v-model="iSettings.searchableAttributes"
+        label="Searchable Attributes"
+        hint="A list of strings, or *"
+        use-input
+        use-chips
+        multiple
+        stack-label
+        hide-dropdown-icon
+        input-debounce="0"
+        new-value-mode="add"
       />
-
+      <q-select
+        filled
+        v-model="iSettings.filterableAttributes"
+        label="Filterable Attributes"
+        hint="A list of strings, or empty"
+        use-input
+        use-chips
+        multiple
+        stack-label
+        hide-dropdown-icon
+        input-debounce="0"
+        new-value-mode="add"
+      />
+      <q-select
+        filled
+        v-model="iSettings.sortableAttributes"
+        label="Sortable Attributes"
+        hint="A list of strings, or empty"
+        use-input
+        use-chips
+        multiple
+        stack-label
+        hide-dropdown-icon
+        input-debounce="0"
+        new-value-mode="add"
+      />
+      <q-select
+        filled
+        v-model="iSettings.rankingRules"
+        label="Ranking Rules"
+        hint="A list of strings, or empty"
+        use-input
+        use-chips
+        multiple
+        stack-label
+        hide-dropdown-icon
+        input-debounce="0"
+        new-value-mode="add"
+      />
+      <q-select
+        filled
+        v-model="iSettings.stopWords"
+        label="Stop Words"
+        hint="A list of strings, or empty"
+        use-input
+        use-chips
+        multiple
+        stack-label
+        hide-dropdown-icon
+        input-debounce="0"
+        new-value-mode="add"
+      />
+      <hr />
+      <q-select
+        filled
+        v-model="iSettingsSynonymKeys"
+        label="Synonyms"
+        hint="A list of words, or empty"
+        use-input
+        use-chips
+        multiple
+        stack-label
+        hide-dropdown-icon
+        input-debounce="0"
+        @new-value="addSynonymParent"
+        @remove="removeSynonym"
+      />
+      <template v-for="(value, key) in iSettings.synonyms" :key="key">
+        <div class="q-ml-xl">
+          <q-select
+            filled
+            v-model="iSettings.synonyms[key]"
+            :label="`Synonyms for ${key}`"
+            :hint="`Words that are synonyms for ${key}`"
+            use-input
+            use-chips
+            multiple
+            stack-label
+            hide-dropdown-icon
+            input-debounce="0"
+            new-value-mode="add"
+          />
+        </div>
+      </template>
+      <hr />
       <div>
         <q-btn label="Submit" type="submit" color="primary" />
       </div>
@@ -41,9 +137,6 @@ import { storeToRefs } from "pinia";
 
 const $q = useQuasar();
 
-const name = ref(null);
-const age = ref(null);
-const accept = ref(false);
 const iSettings = ref({});
 
 const theSettings = useSettingsStore();
@@ -65,6 +158,17 @@ onMounted(async () => {
 const iSettingsString = computed(() => {
   return JSON.stringify(iSettings.value);
 });
+const iSettingsSynonymKeys = computed(() => {
+  return Object.keys(iSettings.value?.synonyms || {});
+});
+const addSynonymParent = (synonymString, done) => {
+  iSettings.value.synonyms[synonymString] = [""];
+  done();
+};
+const removeSynonym = ({ details }) => {
+  console.log(details);
+  delete iSettings.value.synonyms[details.value];
+};
 
 const onSubmit = () => {
   // if (accept.value !== true) {
