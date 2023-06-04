e40144f85388035b86b25406792d14671b150a33
bwilliamson55 <benjaminawilliamson@gmail.com>
2023-06-04 16:46:23 -0400

    Add store actions for settings

--- src/stores/settings-store.js
+++ src/stores/settings-store.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import MeiliSearch from "meilisearch";
 
 export const useSettingsStore = defineStore("settings", {
   state: () => ({
@@ -9,5 +10,46 @@ export const useSettingsStore = defineStore("settings", {
     currentInstance: null,
     instances: [],
   }),
+  actions: {
+    getIndexClient(indexName) {
+      try {
+        const meiliClient = new MeiliSearch({
+          host: this.indexUrl,
+          apiKey: this.indexKey,
+        });
+        return meiliClient.index(indexName);
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+    async getIndexSettings(indexName) {
+      try {
+        const mclient = this.getIndexClient(indexName);
+        let settings = await mclient.getSettings();
+        try {
+          let stats = await mclient.getStats();
+          settings.stats = stats;
+          settings.attributeCodes = Object.keys(stats.fieldDistribution);
+        } catch (error) {
+          console.error(error);
+        }
+        return settings;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+    async getIndexStats(indexName) {
+      try {
+        const mclient = this.getIndexClient(indexName);
+        let stats = await mclient.getStats();
+        return stats;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+  },
   persist: true,
 });
